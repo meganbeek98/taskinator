44e50f988bb680c8a74c07ca6f81ffd8febdd6ae
meganbeek98 <meganbeek98@gmail.com>
2022-03-11 05:46:11 -0500

    event.preventDefault

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-let buttonEl = document.querySelector("#save-task");
+let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
 
-let createTaskHandler = function() {
+let createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     let listItemEl = document.createElement("li");   // Creates a new task item.
     listItemEl.className = "task-item";              // Styles the new task item.
-    listItemEl.textContent = "This is a new task."   // Adds the text.
-    tasksToDoEl.appendChild(listItemEl);            // Appends this element to the task list.
-};                                               // ( Append === inserts content to the end of already existing elements.)
+    listItemEl.textContent = "This is a new task.";   // Adds the text.
+    tasksToDoEl.appendChild(listItemEl);          // Appends this element to the task list. // ( Append === inserts content to the end of already existing elements. 
+};
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
